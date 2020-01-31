98d56500ade9ca06a8b45a67abef2df3e675546b
Viktor-pnkn <tommegan99@gmail.com>
2020-01-31 20:06:42 +0300

    message

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -46,6 +46,7 @@ public class Data {
                 data.form.add(new Form(str[0], Long.parseLong(str[1]), str[2], str[3], str[4], str[5], str[6], str[7], str[8], new SimpleDateFormat("yyyy-dd-MM-HH").parse(str[11])));
             }
         }
+        System.out.println("Parasha");
         return data;
     }
 
